dd5d07157eef1aa8876b39736179ea8128c93b85
Sofya <tarnalitska@gmail.com>
2025-06-18 12:08:44 +0200

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Sofya Tarnalitskaya on 18/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Sofya Tarnalitskaya on 18/06/2025.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
